3026816506bd4b14d7e0205287af67054cd8c714
Andrey M. Mamedov <mam28.andr@yandex.ru>
2025-11-20 17:07:42 +0800

    resolve: окончательное исправление конфигурации doctest в тестах
    Проблема:
    - Отдельные тестовые исполняемые файлы (test_config, test_logger) не линковались
    - Возникали ошибки 'неопределенная ссылка' на функции doctest
    - Отсутствовала функция main() в отдельных тестовых таргетах
    
    Решение:
    - Добавлен target_compile_definitions с DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
      для test_config и test_logger
    - Сохранена архитектура с run_tests для запуска всех тестов вместе
    - Убраны дублирующиеся определения DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN из .cpp файлов
    
    Результат:
    - test_config и test_logger успешно компилируются и линкуются как отдельные исполняемые файлы
    - run_tests собирает все тесты в одном исполняемом файле
    - CTest настроен для запуска как отдельных тестов, так и всех тестов вместе
    - Сборка через Ninja завершается без ошибок

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -1,6 +1,6 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-//#include <doctest/doctest.h>
+//#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+//#include "doctest.h"
+#include <doctest/doctest.h>
 #include "../src/Utils/Config.h"
 #include <filesystem>
 
